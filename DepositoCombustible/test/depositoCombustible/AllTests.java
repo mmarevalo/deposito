96f13651687e1a472d4476a8c17b2f3f79add6ce
miguelmas <Miguel@DESKTOP-1O13VBS>
2021-03-26 10:31:33 +0100

    Suite

---
+++ DepositoCombustible/test/depositoCombustible/AllTests.java
@@ -0,0 +1,11 @@
+package depositoCombustible;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CombustibleTestParam.class, DepositoCombustibleTest.class })
+public class AllTests {
+
+}
